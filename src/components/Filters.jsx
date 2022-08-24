586c0955242e180ce268ff305a20fcf37b8553d6
Ciprian Bolboros <cbolboros@gmail.com>
2022-08-24 10:27:09 +0300

    removed unsed import

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 export default function Filters({ todos, setFilteredTodos, activeFilter, setActiveFilter }) {
   useEffect(() => {
